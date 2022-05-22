844be2e35a8c24e352f8f997c6564b1070eddabc
wen-teng <wenteng2022@163.com>
2022-05-22 22:26:55 +0800

    恢复App.vue和HelloWorld.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <router-view />
+  <img
+    alt="Vue logo"
+    src="./assets/logo.png"
+  />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
 </template>
 
 <style>
@@ -12,5 +19,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
