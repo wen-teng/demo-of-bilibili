54ac6bb7abc14d716ce1c2b29fa580c3b900241a
wen-teng <wenteng2022@163.com>
2022-05-22 21:56:03 +0800

    修复eslint配置对ts文件引入的提示性错误

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,5 +18,26 @@ module.exports = {
   ],
   rules: {
     semi: ['error', 'never'],
+
+    // 修复 - Typescript eslint-缺少文件扩展名“ ts”导入/扩展名
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
+        ts: 'never',
+        tsx: 'never',
+      },
+    ],
+  },
+  settings: {
+    // 修复 - 将eslint与typescript一起使用-无法解析模块的路径
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src/'],
+      },
+    },
   },
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import appRouter from './router/index'
+
 // Normalize.css 是一个可以定制的CSS文件，它让不同的浏览器在渲染网页元素的时候形式更统一。
 import './assets/css/normalize.css'
 
 // 由于HTML标签自带一些CSS属性，导致在调试样式的时候会出现各种奇怪的问题
 import './assets/css/reset.css'
 
-createApp(App).mount('#app')
+createApp(App)
+  .use(appRouter)
+  .mount('#app')
